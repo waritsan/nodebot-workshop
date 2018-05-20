f8df6100cf17f3bff4a5209f4a7dfcb27ab2b5b8
Warit Santaputra <waritsan@gmail.com>
2018-05-20 19:47:14 +0700

    Add ping bell

--- 06-ping-bell.js
+++ 06-ping-bell.js
@@ -1,6 +1,27 @@
 const five = require('johnny-five');
+const dgram = require('dgram');
 const board = five.Board();
+const server = dgram.createSocket('udp4');
 
 board.on('ready', function () {
-  var piezo = five.Piezo();
+  var piezo = new five.Piezo(8);
+  server.on('message', (msg, rinfo) => {
+    piezo.play({
+      song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+      beats: 1 / 4,
+      tempo: 100
+    });
+  });
+  
+  server.on('error', (err) => {
+    console.log(err);
+    server.close();
+  });
+
+  server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+  });
+
+  server.bind(1337);
 });
